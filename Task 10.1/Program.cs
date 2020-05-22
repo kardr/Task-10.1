bb1180f3faef69700cb30fc9d429949d01b6a1dd
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-22 15:06:40 +0300

    .

--- Task 10.1/Program.cs
+++ Task 10.1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Task_10._1
 {
@@ -10,6 +11,35 @@ namespace Task_10._1
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Directory.CreateDirectory("C:\\temp");
+                Directory.CreateDirectory("C:\\temp\\K1");
+                Directory.CreateDirectory("C:\\temp\\K2");
+                StreamWriter sw = new StreamWriter("C:\\temp\\K1\\t1.txt");
+                sw.Write("Иванов Иван Иванович, 1965 года рождения, место жительства г. Саратов");
+                sw.Close();
+                sw = new StreamWriter("C:\\temp\\K1\\t2.txt");
+                sw.Write("Петров Сергей Федорович, 1966 года рождения, место жительства г.Энгельс");
+                sw.Close();
+                sw = new StreamWriter("C:\\temp\\K2\\t3.txt");
+                StreamReader sr = new StreamReader("C:\\temp\\K1\\t1.txt");
+                sw.WriteLine(sr.ReadToEnd());
+                sr.Close();
+                sr = new StreamReader("C:\\temp\\K1\\t2.txt");
+                sw.WriteLine(sr.ReadToEnd());
+                sr.Close();
+                sw.Close();
+                File.Move("C:\\temp\\K1\\t2.txt", "C:\\temp\\K2\\t2.txt");
+                File.Copy("C:\\temp\\K1\\t1.txt", "C:\\temp\\K2\\t1.txt");
+                Directory.Move("C:\\temp\\K2", "C:\\temp\\ALL");
+                Directory.Delete("C:\\temp\\K1", true);
+            }
+            catch(Exception E)
+            {
+                Console.WriteLine(E.Message);
+            }
         }
     }
 }
+
